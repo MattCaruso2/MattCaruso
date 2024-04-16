3c9bc8cefc165f2b5ad358c2466359f08b6cdd11
MattCaruso2 <98111065+MattCaruso2@users.noreply.github.com>
2024-04-16 12:50:22 -0700

    Add files via upload

---
+++ Induction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Induction {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner (System.in);
+System.out.println("Prove that ");
+		
+		
+		
+		
+		
+}//end public
+
+}//end class
